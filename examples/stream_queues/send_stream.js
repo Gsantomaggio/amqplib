a26f8439737c290bf8d12ff4f155ed36d2f383e0
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2022-05-17 18:11:36 +0200

    fix: fix timing async sendToQueue function

--- examples/stream_queues/send_stream.js
+++ examples/stream_queues/send_stream.js
@@ -25,44 +25,12 @@ async function sendStream () {
     const msg = 'Hello World!';
 
     // send the message to the stream queue
-    const _qok = await ok
-    ch.sendToQueue(q, Buffer.from(msg));
+    const _qok = await ok;
+    await ch.sendToQueue(q, Buffer.from(msg));
     console.log(" [x] Sent '%s'", msg);
-    ch.close();
+    await ch.close();
 
     conn.close();
 }
 
 sendStream().catch(console.warn)
-
-
-// amqp.connect('amqp://localhost').then(conn => {
-//
-//     conn.createChannel().then(ch => {
-//
-//         const q = 'my_first_stream';
-//
-//         // Define the queue stream
-//         // Mandatory: exclusive: false, durable: true  autoDelete: false
-//         const ok = ch.assertQueue(q, {
-//             exclusive: false,
-//             durable: true,
-//             autoDelete: false,
-//             arguments: {
-//                 'x-queue-type': 'stream', // Mandatory to define stream queue
-//                 'x-max-length-bytes': 2_000_000_000 // Set the queue retention to 2GB else the stream doesn't have any limit
-//             }
-//         });
-//
-//         const msg = 'Hello World!';
-//
-//         // send the message to the stream queue
-//         ok.then(_qok => {
-//             ch.sendToQueue(q, Buffer.from(msg));
-//             console.log(" [x] Sent '%s'", msg);
-//             ch.close();
-//         });
-//
-//     }).finally(() => { conn.close(); });
-//
-// }).catch(console.warn);
