b2e4f3cb1b4936bd7fe8f50cc94e329807fc12e1
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2022-05-18 12:36:06 +0200

    feat: add function error checking and function export

--- examples/stream_queues/receive_stream.js
+++ examples/stream_queues/receive_stream.js
@@ -2,41 +2,53 @@
 
 const amqp = require('amqplib');
 
-async function receiveStream() {
+async function receiveStream() { //=====================================================================================
 
-    const conn = await amqp.connect('amqp://localhost');
-    process.once('SIGINT', () => { conn.close(); });
+    try {
 
-    const ch = await conn.createChannel();
+        const conn = await amqp.connect('amqp://localhost');
+        process.once('SIGINT', () => {
+            conn.close();
+        });
 
-    const q = 'my_first_stream';
+        const ch = await conn.createChannel();
+        const q = 'my_first_stream';
 
-    // Define the queue stream
-    // Mandatory: exclusive: false, durable: true  autoDelete: false
-    await ch.assertQueue(q, {
-        exclusive: false,
-        durable: true,
-        autoDelete: false,
-        arguments: {
-            'x-queue-type': 'stream', // Mandatory to define stream queue
-            'x-max-length-bytes': 2_000_000_000 // Set the queue retention to 2GB else the stream doesn't have any limit
-        }
-    });
+        // Define the queue stream
+        // Mandatory: exclusive: false, durable: true  autoDelete: false
+        await ch.assertQueue(q, {
+            exclusive: false,
+            durable: true,
+            autoDelete: false,
+            arguments: {
+                'x-queue-type': 'stream', // Mandatory to define stream queue
+                'x-max-length-bytes': 2_000_000_000 // Set the queue retention to 2GB else the stream doesn't have any limit
+            }
+        });
 
-    ch.qos(100); // this is mandatory
+        ch.qos(100); // this is mandatory
 
-    ch.consume(q, (msg) => {
-        console.log(" [x] Received '%s'", msg.content.toString());
-        ch.ack(msg); // mandatory
-    }, {
-        noAck: false,
-        arguments: {
-            'x-stream-offset': 'first' // here you can specify the offset: : first, last, next, and timestamp
-            // with first start consuming always from the beginning
-        }
-    });
+        ch.consume(q, (msg) => {
+            console.log(" [x] Received '%s'", msg.content.toString());
+            ch.ack(msg); // mandatory
+        }, {
+            noAck: false,
+            arguments: {
+                'x-stream-offset': 'first' // here you can specify the offset: : first, last, next, and timestamp
+                // with first start consuming always from the beginning
+            }
+        });
 
-    console.log(' [*] Waiting for messages. To exit press CTRL+C');
+        console.log(' [*] Waiting for messages. To exit press CTRL+C');
+
+    }
+    // Catch and display any errors in the console
+    catch(e) { console.log(e) }
+}
+//======================================================================================================================
+
+module.exports = {
+    receiveStream
 }
 
-receiveStream().catch(console.warn);
+// To test call the function: receiveStream()
--- examples/stream_queues/send_stream.js
+++ examples/stream_queues/send_stream.js
@@ -2,34 +2,44 @@
 
 const amqp = require('amqplib');
 
-async function sendStream () {
 
-    const conn = await amqp.connect('amqp://localhost');
+async function sendStream () { //=======================================================================================
 
-    const ch = await conn.createChannel();
+    try {
 
-    const q = 'my_first_stream';
+        const conn = await amqp.connect('amqp://localhost');
+        const ch = await conn.createChannel();
+        const q = 'my_first_stream';
+        const msg = 'Hello World!';
 
-    // Define the queue stream
-    // Mandatory: exclusive: false, durable: true  autoDelete: false
-    await ch.assertQueue(q, {
-        exclusive: false,
-        durable: true,
-        autoDelete: false,
-        arguments: {
-            'x-queue-type': 'stream', // Mandatory to define stream queue
-            'x-max-length-bytes': 2_000_000_000 // Set the queue retention to 2GB else the stream doesn't have any limit
-        }
-    });
+        // Define the queue stream
+        // Mandatory: exclusive: false, durable: true  autoDelete: false
+        await ch.assertQueue(q, {
+            exclusive: false,
+            durable: true,
+            autoDelete: false,
+            arguments: {
+                'x-queue-type': 'stream', // Mandatory to define stream queue
+                'x-max-length-bytes': 2_000_000_000 // Set the queue retention to 2GB else the stream doesn't have any limit
+            }
+        });
 
-    const msg = 'Hello World!';
+        // Send the message to the stream queue
+        await ch.sendToQueue(q, Buffer.from(msg));
+        console.log(" [x] Sent '%s'", msg);
+        await ch.close();
 
-    // Send the message to the stream queue
-    await ch.sendToQueue(q, Buffer.from(msg));
-    console.log(" [x] Sent '%s'", msg);
-    await ch.close();
+        // Close connection
+        conn.close();
 
-    conn.close();
+    }
+    // Catch and display any errors in the console
+    catch(e) { console.log(e) }
+}
+//======================================================================================================================
+
+module.exports = {
+    sendStream
 }
 
-sendStream().catch(console.warn)
+// To test call the function: sendStream()
